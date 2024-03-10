2894e9f4421476519a51a675a6b7103217b4fd6c
Rishabh Chopde <rishikumarikuf@gmail.com>
2024-03-11 03:06:58 +0530

    Working syntax intellisense

--- esp32-tasks/udp-client/main/udp_client.c
+++ esp32-tasks/udp-client/main/udp_client.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include <string.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void) {
+    ESP_LOGI("Hello: HI", "does this work");
 
+    return;
 }
